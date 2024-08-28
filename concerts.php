cc72e379bcf48e731de4a7aeb1a88cc3a6fb0c72
Vincent Jacquelin <vjacquelinpro@gmail.com>
2024-08-28 11:22:30 +0200

    Résolution du problème de merge

---
+++ concerts.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vaincent: Ténor Opéra - Pop</title>
+
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Fonts -->
+    <?php require "font.php" ?>
+    
+    <script src="script.js" defer></script>
+    
+</head>
+
+
+
+<body class="affichage-off">
+
+    <main id>
+
+    <?php require "header.php" ?>
+
+    <div id="correction-page-concerts">
+
+         <?php require "table-concerts.php" ?>
+
+    </div>
+
+    <?php require "footer.php" ?>
+    
+
+</main>
+</body>
+</html>
